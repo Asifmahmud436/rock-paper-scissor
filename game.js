cef21f164b5e0b4825a09032ea6f44bcce7fe9e0
Zettex <safaandsafa4@gmail.com>
2024-07-31 12:07:07 +0600

    Buttons with DOM added

--- game.js
+++ game.js
@@ -1,9 +1,39 @@
-// alert("The project is initiated!");
-function  getHumanChoice(){
-    let str1 = prompt("3,2,1....! : ");
-    return str1.toLowerCase();
-}
+// button section
+const container = document.querySelector('#container');
 
+const btn1 = document.createElement('button');
+btn1.classList.add('btn1');
+btn1.innerText = "rock";
+const btn2 = document.createElement('button');
+btn2.classList.add('btn2');
+btn2.innerText = "paper";
+const btn3 = document.createElement('button');
+btn3.classList.add('btn3');
+btn3.innerText = "scissor";
+
+container.appendChild(btn1);
+container.appendChild(btn2);
+container.appendChild(btn3);
+
+// PARAGRAPH
+const containerTwo = document.querySelector('#container-two');
+const p = document.createElement('p');
+p.innerText = 'Play Against AI';
+containerTwo.appendChild(p);
+
+
+// btn1.addEventListener("click", function (e) {
+//     console.log("Working");
+// });
+
+
+
+// to get the human choice
+// function  getHumanChoice(){
+//     let str1 = prompt("3,2,1....! : ");
+//     return str1.toLowerCase();
+// }
+// to get a random computer choice
 function getComputerChoice(){
     choices = ['rock','paper','scissors'];
     return  choices[Math.floor(Math.random()*3)];
@@ -11,81 +41,245 @@ function getComputerChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
-
-function playRound(humanSelection,computerSelection){
+let rounds = 0;
+// function playRound(){
+//     // humanSelection = humanSelection.toLowerCase();
+//     let humanSelection = btn1.innerText;
+//     let computerSelection = getComputerChoice();
+//     p.innerText = ``;
+//     if(humanSelection === 'rock'){
+//         if(computerSelection === 'paper'){
+//             p.innerText = `You lose! ${computerSelection} beats ${humanSelection}`;
+//             computerScore++;
+//         }
+//         else if(computerSelection === 'scissors'){
+//             p.innerText = `You Win! ${humanSelection} beats ${computerSelection}`;
+//             humanScore++;
+//         }
+//         else{
+//             p.innerText = `Its a DRAW!`;
+//         }
+//     }
+//     else if(humanSelection === 'paper'){
+//         if(computerSelection === 'scissor'){
+//             p.innerText = `You lose! ${computerSelection} beats ${humanSelection}`;
+//             computerScore++;
+//         }
+//         else if(computerSelection === 'rock'){
+//             p.innerText = `You Win! ${humanSelection} beats ${computerSelection}`;
+//             humanScore++;
+//         }
+//         else{
+//             p.innerText = `Its a DRAW!`;
+//         }
+//     }
+//     else{
+//         if(computerSelection === 'rock'){
+//             p.innerText = `You lose! ${computerSelection} beats ${humanSelection}`;
+//             computerScore++;
+//         }
+//         else if(computerSelection === 'paper'){
+//             p.innerText = `You Win! ${humanSelection} beats ${computerSelection}`;
+//             humanScore++;
+//         }
+//         else{
+//             p.innerText = `Its a DRAW!`;
+//         }
+//     }
+// }
+btn1.addEventListener('click',function (){
     // humanSelection = humanSelection.toLowerCase();
+    rounds++;
+    let humanSelection = btn1.innerText;
+    let computerSelection = getComputerChoice();
+    p.innerText = ``;
     if(humanSelection === 'rock'){
         if(computerSelection === 'paper'){
-            console.log(`You lose! ${computerSelection} beats ${humanSelection}`)
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
             computerScore++;
         }
         else if(computerSelection === 'scissors'){
-            console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
             humanScore++;
         }
         else{
-            console.log(`Its a DRAW!`);
+            p.innerText = `Its a DRAW!`;
         }
     }
     else if(humanSelection === 'paper'){
         if(computerSelection === 'scissor'){
-            console.log(`You lose! ${computerSelection} beats ${humanSelection}`)
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
             computerScore++;
         }
         else if(computerSelection === 'rock'){
-            console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
             humanScore++;
         }
         else{
-            console.log(`Its a DRAW!`);
+            p.innerText = `Its a DRAW!`;
         }
     }
     else{
         if(computerSelection === 'rock'){
-            console.log(`You lose! ${computerSelection} beats ${humanSelection}`)
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
             computerScore++;
         }
         else if(computerSelection === 'paper'){
-            console.log(`You Win! ${humanSelection} beats ${computerSelection}`)
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
             humanScore++;
         }
         else{
-            console.log(`Its a DRAW!`);
+            p.innerText = `Its a DRAW!`;
         }
     }
-}
-
-function playGame(){
-    let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    console.log(humanScore,computerScore);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    console.log(humanScore,computerScore);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    console.log(humanScore,computerScore);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    console.log(humanScore,computerScore);
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection,computerSelection);
-    console.log(humanScore,computerScore);
-
-    if(humanScore>computerScore){
-        alert("You defeated the easiest bot made by me ! Well Played,Noob ! ")
+    if(rounds>0 && rounds%5==0){
+        if(humanScore>computerScore){
+            p.innerText += " 5 rounds over.You defeated the easiest bot made by me ! Well Played,Noob ! ";
+        }
+        else if(humanScore<computerScore){
+            p.innerText +=  " 5 rounds over.You lost to the most easieast bot ??? Hell nawwwwwwh...";
+        }
+        else{
+            p.innerText += " 5 rounds over.You didnt win or lose . But you sure wastes your time!";
+        }       
     }
-    else if(humanScore<computerScore){
-        alert("You lost to the most easieast bot ??? Hell nawwwwwwh...")
+});
+
+btn2.addEventListener('click',function (){
+    // humanSelection = humanSelection.toLowerCase();
+    rounds++;
+    let humanSelection = btn2.innerText;
+    let computerSelection = getComputerChoice();
+    p.innerText = ``;
+    if(humanSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'scissors'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
+    }
+    else if(humanSelection === 'paper'){
+        if(computerSelection === 'scissor'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'rock'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
     }
     else{
-        alert("You didnt win or lose . But you sure wastes your time!")
+        if(computerSelection === 'rock'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'paper'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
     }
-}
+    if(rounds>0 && rounds%5==0){
+        if(humanScore>computerScore){
+            p.innerText += " 5 rounds over.You defeated the easiest bot made by me ! Well Played,Noob ! ";
+        }
+        else if(humanScore<computerScore){
+            p.innerText += " 5 rounds over.You lost to the most easieast bot ??? Hell nawwwwwwh...";
+        }
+        else{
+            p.innerText += " 5 rounds over.You didnt win or lose . But you sure wastes your time!";
+        }       
+    }
+});
 
-playGame();
+btn3.addEventListener('click',function (){
+    // humanSelection = humanSelection.toLowerCase();
+    rounds++;
+    let humanSelection = btn3.innerText;
+    let computerSelection = getComputerChoice();
+    p.innerText = ``;
+    if(humanSelection === 'rock'){
+        if(computerSelection === 'paper'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'scissors'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
+    }
+    else if(humanSelection === 'paper'){
+        if(computerSelection === 'scissor'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'rock'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
+    }
+    else{
+        if(computerSelection === 'rock'){
+            p.innerText = `You lose! ${computerSelection} beats ${humanSelection}.`;
+            computerScore++;
+        }
+        else if(computerSelection === 'paper'){
+            p.innerText = `You Win! ${humanSelection} beats ${computerSelection}.`;
+            humanScore++;
+        }
+        else{
+            p.innerText = `Its a DRAW!`;
+        }
+    }
+    if(rounds>0 && rounds%5==0){
+        if(humanScore>computerScore){
+            p.innerText += " 5 rounds over.You defeated the easiest bot made by me ! Well Played,Noob ! ";
+        }
+        else if(humanScore<computerScore){
+            p.innerText += " 5 rounds over.You lost to the most easieast bot ??? Hell nawwwwwwh...";
+        }
+        else{
+            p.innerText += " 5 rounds over.You didnt win or lose . But you sure wastes your time!";
+        }       
+    }
+});
+
+// function playGame(){
+//     let humanSelection = getHumanChoice();
+//     let computerSelection = getComputerChoice();
+//     playRound(humanSelection,computerSelection);
+    
+
+//     if(humanScore>computerScore){
+//         p.innerText = "You defeated the easiest bot made by me ! Well Played,Noob ! ";
+//     }
+//     else if(humanScore<computerScore){
+//         p.innerText = "You lost to the most easieast bot ??? Hell nawwwwwwh...";
+//     }
+//     else{
+//         p.innerText = "You didnt win or lose . But you sure wastes your time!";
+//     }
+// }
+
+// playGame();
+// btn1.addEventListener('click',getHumanChoice);
+// btn2.addEventListener('click',playRound);
+// btn3.addEventListener('click',playGame);
+
